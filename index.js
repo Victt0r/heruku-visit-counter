657470cb2d4a7877e730b99821444664ff186704
Victt0r <dsnobik@gmail.com>
2019-08-20 11:59:45 +0300

    setInterval

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var count = 0
 const PORT = process.env.PORT || 3000
-require("http").createServer(
+var http = require("http")
+
+http.createServer(
   (request, response) => {
     if (request.url == '/favicon.ico') return response.end('')
     if (request.url == '/') 
@@ -9,3 +11,6 @@ require("http").createServer(
   }
 ).listen(PORT)
 
+setInterval(function() {
+  http.get("http://heruku-visit-counter.herokuapp.com/count")
+}, 29*60000); // every 29 minutes 
