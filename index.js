aa1c9aeb456afe79d7dc76ab8c6edd6634282698
Victt0r <dsnobik@gmail.com>
2019-09-04 14:02:28 +0300

    mongodb counter

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var count = 0
 const PORT = process.env.PORT || 3000
-var http = require("http")
+const http = require("http")
+
+mongoConnectStr = 'mongodb+srv://user1:N134bmOcJYMqRX4D@cluster007-gphrw.gcp.mongodb.net/test?retryWrites=true&w=majority'
+
+var db, coll
+
+const MongoClient = require("mongodb").MongoClient
+const mongoClient = new MongoClient(mongoConnectStr, { useNewUrlParser: true });
+mongoClient.connect(async function (err, client) {
+
+  if (err) {
+    return console.log(err);
+  }
+  // тут взаимодействие с базой данных
+  db = client.db("counter_db")
+  coll = db.collection("counter_coll")
+  count = (await coll.findOne({ id: 1 })).count
+
+})
 
 http.createServer(
   (request, response) => {
     if (request.url == '/favicon.ico') return response.end('')
-    if (request.url == '/') 
-      return response.end(`You are the visitor number ${++count}.`)
-    if (request.url == '/count') return response.end(String(++count))
+    if (request.url == '/') {
+      response.end(`You are the visitor number ${++count}.`)
+      coll.updateOne({ id: 1 }, { $set: { count: count } })
+    }
+    if (request.url == '/count') {
+      response.end(String(++count))
+      coll.updateOne({ id: 1 }, { $set: { count: count } })
+    }
+
   }
 ).listen(PORT)
 
-setInterval(function() {
+
+
+setInterval(function () {
   http.get("http://heruku-visit-counter.herokuapp.com/count")
-}, 29*60000); // every 29 minutes 
+}, 29 * 60000); // every 29 minutes 
